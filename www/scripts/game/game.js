2b160f28165b0fc3c869ee8707cc663ffea69ff6
Mikael Brassman <mishakun@gmail.com>
2013-09-14 15:10:02 +0200

    Squish those guys. For the sake of hilarity

--- www/scripts/game/game.js
+++ www/scripts/game/game.js
@@ -11,6 +11,14 @@ define(['layer', 'random'], function(layers, r) {
 		for (i = 0; i < 5; i++) {
 			l.drawSprite(1, 0, 16*r.getRandomInt(0, 24), 16*r.getRandomInt(0,24));
 		};
+		
+		var canvas = document.getElementById('game');
+		canvas.addEventListener('click', function(evt) {
+			var x = evt.offsetX - canvas.offsetLeft + 8;
+			var y = evt.offsetY - canvas.offsetTop - 8;
+			l.drawSprite(1, 1, x, y);
+		});
+
 	});
 	
 });
